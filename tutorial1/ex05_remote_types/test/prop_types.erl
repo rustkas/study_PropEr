14ebba3ae9d74b9566ae3b19bb41c6e4db7333d7
Anatolii Kosorukov <java1cprog@yandex.ru>
2021-07-23 08:12:23 +0300

    update

--- tutorial1/ex05_remote_types/test/prop_types.erl
+++ tutorial1/ex05_remote_types/test/prop_types.erl
@@ -2,6 +2,16 @@
 
 -include_lib("proper/include/proper.hrl").
 
+%% We want to test that array:new/0 can handle any combination of options.
+%% Instead of writing a custom generator (which may rot), we can use the type
+%% in that file:
+
+%% from array.erl:
+%% -type array_opt() :: 'fixed' | non_neg_integer()
+%% 		   | {'default', term()} | {'fixed', boolean()}
+%% 		   | {'size', non_neg_integer()}.
+%% -type array_opts() :: array_opt() | [array_opt()].
+
 %%%%%%%%%%%%%%%%%%
 %%% Properties %%%
 %%%%%%%%%%%%%%%%%%
